154d06c7ff1ef6c95520e1fdb580d4ded6dbb522
Navathakamaraj <navathakamaraj2006@gmail.com>
2026-05-04 18:56:19 +0530

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean searchMatrix(int[][] m, int t) {
+        for(int i=0;i<m.length;i++){
+            for(int j=0;j<m[i].length;j++){
+                if(m[i][j]==t)
+                return true;
+            }
+        }
+        return false;
+    }
+}
